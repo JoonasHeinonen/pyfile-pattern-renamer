b70a5363f0d6ce7ddba71bfed23a936d2ef3e07a
Joonas Heinonen <mrdanglars@gmail.com>
2026-05-31 11:59:24 +0300

    python-pattern-renamer: Defined functions

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-folder = input("Enter the repository folder: ")
+directory = ""
+file = ""
 
 def main():
-    print("Files in the folder \"" + folder + "\" has been renamed to lowercase.")
+    #Main function that calls the directory_file and read_file functions to read a file from a directory.
+    directory_file(file, directory)
+    read_file()
+
+def directory_file(f, d):
+    #Opens a file from a directory and reads it.
+
+    #Parameters:
+    #    f (str):The name of the file to be opened.
+    #    d (str):The directory where the file is located.
+
+    global directory, file
+    d = input("Enter the repository directory: ")
+    f = input("Enter the file name: ")
+
+    directory = d
+    file = f
+
+def read_file():
+    #Reads the file that was opened in the directory_file function.
+    f = open("C:\\" + directory + "\\" + file)
+    print(f.read())
 
 main()
