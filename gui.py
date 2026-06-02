bc6026b8452f4905325b95b3078b902e2ae09c24
Joonas Heinonen <mrdanglars@gmail.com>
2026-06-02 15:36:41 +0300

    pyfile-pattern-renamer: Input implemented

--- gui.py
+++ gui.py
@@ -1,13 +1,15 @@
 import pygame
 from icon import Icon
 from files import Files
+clock = pygame.time.Clock()
 
 class GUI:
 
-    def __init__(self, folder_files=None, directory=None, font_size=36):
+    def __init__(self, folder_files=None, directory=None, font_size=36, text=""):
         self.folder_files = folder_files
         self.directory = directory
         self.font_size = font_size
+        self.text = text
         print("GUI initialized")
 
     def update_files_view(self, y_offset=45, window_surface=None):
@@ -88,7 +90,6 @@ class GUI:
         else:
             pygame.draw.rect(window_surface, pygame.Color("#424242"), [160, 1, 38, 38])
 
-
         # Folder text field.
         pygame.draw.rect(window_surface, pygame.Color("#001AFF"), [199, 0, window_size[0] - (199 + 40), 40])
         pygame.draw.rect(window_surface, pygame.Color("#3549FF"), [200, 1, window_size[0] - (199 + 42), 18])
@@ -103,7 +104,7 @@ class GUI:
         window_surface.blit(snake_case_icon.render_icon()[0], (160, 0))
         window_surface.blit(directory_text, (205, 5))
 
-        self.button_clicks(mouse, window_size, window_surface)
+        self.button_clicks(mouse, window_size=window_size, window_surface=window_surface)
 
     def run(self):
         pygame.init()
@@ -122,6 +123,13 @@ class GUI:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     is_running = False
+                if event.type == pygame.KEYDOWN:
+                    print("Key pressed:", event.unicode)
+                    if event.key == pygame.K_BACKSPACE:
+                        self.directory = self.directory[:-1]
+                    else:
+                        self.directory += event.unicode
+                        print("Directory: ", self.directory)
 
             # stores the (x,y) coordinates into
             # the variable as a tuple
@@ -129,5 +137,12 @@ class GUI:
             
             self.draw_buttons(window_surface, mouse, WINDOW_SIZE)
 
+            input_box = pygame.Rect(10, 200, 620, 100)
+            pygame.draw.rect(window_surface, (255, 255, 255), input_box, 2)
+            font = pygame.font.Font(None, 12)
+            text_surface = font.render(self.directory, True, (255, 255, 255))
+            window_surface.blit(text_surface, (input_box.x + 5, input_box.y + 5))
+
             # updates the frames of the game
-            pygame.display.update()
+            pygame.display.flip()
+            clock.tick(60)
