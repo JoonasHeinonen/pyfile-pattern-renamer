5f846888196558ef9bdee1fce05d4fa13bb04350
Joonas Heinonen <mrdanglars@gmail.com>
2026-06-02 13:51:44 +0300

    pyfile-pattern-renamer: GUI enhanced for buttons

--- gui.py
+++ gui.py
@@ -1,5 +1,4 @@
 import pygame
-import sys
 from icon import Icon
 
 class GUI:
@@ -20,6 +19,25 @@ class GUI:
             window_surface.blit(list_item, (10, y_offset))
             y_offset += 30
 
+    def button_clicks(self, mouse, window_size=(640, 480)):
+        for ev in pygame.event.get():
+            if ev.type == pygame.QUIT:
+                pygame.quit()
+
+            if ev.type == pygame.MOUSEBUTTONDOWN:
+                # Convert button.
+                if 0 <= mouse[0] <= 119 and 0 <= mouse[1] <= 39:
+                    print("Convert button clicked")
+                # Folder button.
+                if window_size[0] - 40 <= mouse[0] <= window_size[0] and 0 <= mouse[1] <= 40:
+                    print("Folder button clicked")
+                # Camel case button.
+                if 120 <= mouse[0] <= 159 and 0 <= mouse[1] <= 39:
+                    print("Camel case button clicked")
+                # Snake case button.
+                if 160 <= mouse[0] <= 199 and 0 <= mouse[1] <= 39:
+                    print("Snake case button clicked")
+
     def draw_buttons(self, files, window_surface, mouse, window_size=(640, 480)):
         folder_icon = Icon("media/icon_folder.png")
         camel_case_icon = Icon("media/icon_camel_case.png")
@@ -31,34 +49,37 @@ class GUI:
         text = font.render("Convert", True, pygame.Color("#FFFFFF"))
         directory_text = small_font.render(self.directory, True, pygame.Color("#FFFFFF"))
 
-        # Normalize button.
-        if 0 <= mouse[0] <= 120 and 0 <= mouse[1] <= 40:
-            pygame.draw.rect(window_surface, pygame.Color("#CCCCCC"), [0, 0, 120, 40])
-            pygame.draw.rect(window_surface, pygame.Color("#CCCCCC"), [window_size[0] - 40, 0, 40, 40])
+
+        pygame.draw.rect(window_surface, pygame.Color("#222222"), [0, 0, window_size[0], 40])
+        
+        # Convert button.
+        if 0 <= mouse[0] <= 119 and 0 <= mouse[1] <= 39:
+            pygame.draw.rect(window_surface, pygame.Color("#CCCCCC"), [1, 1, 119, 38])
         else:
-            pygame.draw.rect(window_surface, pygame.Color("#424242"), [0, 0, 120, 40])
-            pygame.draw.rect(window_surface, pygame.Color("#424242"), [window_size[0] - 40, 0, 40, 40])
+            pygame.draw.rect(window_surface, pygame.Color("#424242"), [1, 1, 119, 38])
         
         # Folder button.
         if window_size[0] - 40 <= mouse[0] <= window_size[0] and 0 <= mouse[1] <= 40:
-            pygame.draw.rect(window_surface, pygame.Color("#CCCCCC"), [window_size[0] - 40, 0, 40, 40])
+            pygame.draw.rect(window_surface, pygame.Color("#CCCCCC"), [window_size[0] - 39, 1, 38, 38])
         else:
-            pygame.draw.rect(window_surface, pygame.Color("#424242"), [window_size[0] - 40, 0, 40, 40])
+            pygame.draw.rect(window_surface, pygame.Color("#424242"), [window_size[0] - 39, 1, 38, 38])
         
         # Camel case button.
-        if 120 <= mouse[0] <= 160 and 0 <= mouse[1] <= 40:
-            pygame.draw.rect(window_surface, pygame.Color("#CCCCCC"), [120, 0, 40, 40])
+        if 120 <= mouse[0] <= 159 and 0 <= mouse[1] <= 39:
+            pygame.draw.rect(window_surface, pygame.Color("#CCCCCC"), [121, 1, 38, 38])
         else:
-            pygame.draw.rect(window_surface, pygame.Color("#424242"), [120, 0, 40, 40])
+            pygame.draw.rect(window_surface, pygame.Color("#424242"), [121, 1, 38, 38])
         
         # Snake case button.
-        if 160 <= mouse[0] <= 200 and 0 <= mouse[1] <= 40:
-            pygame.draw.rect(window_surface, pygame.Color("#CCCCCC"), [160, 0, 40, 40])
+        if 160 <= mouse[0] <= 199 and 0 <= mouse[1] <= 39:
+            pygame.draw.rect(window_surface, pygame.Color("#CCCCCC"), [160, 1, 38, 38])
         else:
-            pygame.draw.rect(window_surface, pygame.Color("#424242"), [160, 0, 40, 40])
+            pygame.draw.rect(window_surface, pygame.Color("#424242"), [160, 1, 38, 38])
 
-        pygame.draw.rect(window_surface, pygame.Color("#001AFF"), [200, 0, window_size[0] - (200 + 40), 20], 1)
-        pygame.draw.rect(window_surface, pygame.Color("#3549FF"), [201, 1, window_size[0] - (200 + 42), 18])
+
+        # Folder text field.
+        pygame.draw.rect(window_surface, pygame.Color("#001AFF"), [199, 0, window_size[0] - (199 + 40), 40])
+        pygame.draw.rect(window_surface, pygame.Color("#3549FF"), [200, 1, window_size[0] - (199 + 42), 18])
 
         # superimposing the text onto our button
         window_surface.blit(text, (10, 10))
@@ -69,6 +90,8 @@ class GUI:
         window_surface.blit(camel_case_icon.render_icon()[0], (120, 0))
         window_surface.blit(snake_case_icon.render_icon()[0], (160, 0))
         window_surface.blit(directory_text, (205, 5))
+
+        self.button_clicks(mouse, window_size)
 
     def run(self):
         pygame.init()
@@ -91,9 +114,6 @@ class GUI:
             # stores the (x,y) coordinates into
             # the variable as a tuple
             mouse = pygame.mouse.get_pos()
-
-            # if mouse is hovered on a button it
-            # changes to lighter shade 
             
             self.draw_buttons(files, window_surface, mouse, WINDOW_SIZE)
 
